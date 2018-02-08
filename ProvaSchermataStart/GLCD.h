0c350f946ef4acaa4e5d59568c3bd0df9cfc363d
Arenno <alessandrorennola@gmail.com>
2018-02-08 10:52:33 +0100

    Driver Choice Eliminated

--- ProvaSchermataStart/GLCD.h
+++ ProvaSchermataStart/GLCD.h
@@ -1,13 +1,3 @@
-/******************************************************************************/
-/* GLCD.h: Graphic LCD function prototypes and defines                        */
-/******************************************************************************/
-/* This file is part of the uVision/ARM development tools.                    */
-/* Copyright (c) 2005-2009 Keil Software. All rights reserved.                */
-/* This software may only be used under the terms of a valid, current,        */
-/* end user licence from KEIL for a compatible version of KEIL software       */
-/* development tools. Nothing else gives you the right to use this software.  */
-/******************************************************************************/
-
 #ifndef _GLCD_H
 #define _GLCD_H
 
--- ProvaSchermataStart/GLCD_LPC1700.c
+++ ProvaSchermataStart/GLCD_LPC1700.c
@@ -59,7 +59,7 @@
 
 /******************************************************************************/
 static volatile unsigned short TextColor = Black, BackColor = White;
-static unsigned short driverCode;
+
 
 /************************ Local auxiliary functions ***************************/
 
@@ -105,26 +105,6 @@ static __inline unsigned char lcd_send (unsigned short byte) {
 
 
 /*******************************************************************************
-* read 1 byte over serial communication                                        *
-*   Parameter:    byte:   byte to be sent                                      *
-*   Return:                                                                    *
-*******************************************************************************/
-
-static __inline unsigned short lcd_read (void) {
-  unsigned short id;
-  LPC_GPIO2->FIODIR &= 0xffffff00;                //P2.0...P2.7 Input
-  LCD_DIR(0)		   				              //Interface B->A
-  LCD_EN(0)	                                      //Enable 2B->2A
-  wait_delay(80);							      //delay some times
-  id = LPC_GPIO2->FIOPIN & 0x00ff;                //Read D8..D15                         
-  LCD_EN(1)	                                      //Enable 1B->1A
-  wait_delay(80);							      //delay some times
-  id = (id << 8) | (LPC_GPIO2->FIOPIN & 0x00ff);  //Read D0..D7                         
-  LCD_DIR(1)						
-  return(id); 
-}
-
-/*******************************************************************************
 * Write command to LCD controller                                              *
 *   Parameter:    c:      command to be written                                *
 *   Return:                                                                    *
@@ -155,23 +135,6 @@ static __inline void wr_dat (unsigned short c) {
   LCD_WR(0)
   wait();
   LCD_WR(1)
-}
-
-/*******************************************************************************
-* Read data from LCD controller                                                *
-*   Parameter:                                                                 *
-*   Return:               read data                                            *
-*******************************************************************************/
-
-static __inline unsigned short rd_dat (void) {
-  unsigned short val = 0;
-
-  LCD_RS(1)
-  LCD_WR(1)
-  LCD_RD(0)
-  val = lcd_read();
-  LCD_RD(1)
-  return val;
 }
 
 /*******************************************************************************
@@ -229,25 +192,6 @@ static __inline void wr_reg (unsigned char reg, unsigned short val) {
 
 
 /*******************************************************************************
-* Read from LCD register                                                       *
-*   Parameter:    reg:    register to be read                                  *
-*   Return:               value read from register                             *
-*******************************************************************************/
-
-static unsigned short rd_reg (unsigned short reg) {
-  unsigned short val = 0;
-
-  LCD_CS(0)
-  wr_cmd(reg);
-  val = rd_dat(); 
-  LCD_CS(1)
-  return (val);
-}
-
-
-/************************ Exported functions **********************************/
-
-/*******************************************************************************
 * Initialize the Graphic LCD controller                                        *
 *   Parameter:                                                                 *
 *   Return:                                                                    *
@@ -260,242 +204,81 @@ void GLCD_Init (void) {
   LPC_GPIO0->FIOSET    = 0x03f80000;	
 
   delay(5);                             /* Delay 50 ms                        */
+	//driverCode = 0x9325
+  wr_reg(0x00e7,0x0010);      
+  wr_reg(0x0000,0x0001);  			//start internal osc
+  wr_reg(0x0001,0x0100);     
+  wr_reg(0x0002,0x0700); 				//power on sequence                     
+  wr_reg(0x0003,(1<<12)|(1<<5)|(1<<4) ); 	//65K 
+  wr_reg(0x0004,0x0000);                                   
+  wr_reg(0x0008,0x0207);	           
+  wr_reg(0x0009,0x0000);         
+  wr_reg(0x000a,0x0000); 				//display setting         
+  wr_reg(0x000c,0x0001);				//display setting          
+  wr_reg(0x000d,0x0000); 				//0f3c          
+  wr_reg(0x000f,0x0000);
+	//Power On sequence //
+  wr_reg(0x0010,0x0000);   
+  wr_reg(0x0011,0x0007);
+  wr_reg(0x0012,0x0000);                                                                 
+  wr_reg(0x0013,0x0000);                 
+  delay(15);
+  wr_reg(0x0010,0x1590);   
+  wr_reg(0x0011,0x0227);
+  delay(15);
+  wr_reg(0x0012,0x009c);                  
+  delay(15);
+  wr_reg(0x0013,0x1900);   
+  wr_reg(0x0029,0x0023);
+  wr_reg(0x002b,0x000e);
+  delay(15);
+  wr_reg(0x0020,0x0000);                                                            
+  wr_reg(0x0021,0x0000);           
 
-  driverCode = rd_reg(0x00);
 
-  if(driverCode == 0x4531)				//2.8" TFT LCD Module,DriverIC is LGDP4531
-  {
-  	wr_reg(0x00,0x0001);
-  	wr_reg(0x10,0x0628);
-  	wr_reg(0x12,0x0006);
-  	wr_reg(0x13,0x0A32);
-  	wr_reg(0x11,0x0040);
-  	wr_reg(0x15,0x0050);
-  	wr_reg(0x12,0x0016);
-  	delay(15);
-  	wr_reg(0x10,0x5660);
-  	delay(15);
-  	wr_reg(0x13,0x2A4E);
-  	wr_reg(0x01,0x0100);
-  	wr_reg(0x02,0x0300);
-	
-  	wr_reg(0x03,0x1030);
-	
-  	wr_reg(0x08,0x0202);
-  	wr_reg(0x0A,0x0000);
-  	wr_reg(0x30,0x0000);
-  	wr_reg(0x31,0x0402);
-  	wr_reg(0x32,0x0106);
-  	wr_reg(0x33,0x0700);
-  	wr_reg(0x34,0x0104);
-  	wr_reg(0x35,0x0301);
-  	wr_reg(0x36,0x0707);
-  	wr_reg(0x37,0x0305);
-  	wr_reg(0x38,0x0208);
-  	wr_reg(0x39,0x0F0B);
-  	delay(15);
-  	wr_reg(0x41,0x0002);
-  	wr_reg(0x60,0x2700);
-  	wr_reg(0x61,0x0001);
-  	wr_reg(0x90,0x0119);
-  	wr_reg(0x92,0x010A);
-  	wr_reg(0x93,0x0004);
-  	wr_reg(0xA0,0x0100);
-  	delay(15);
-  	wr_reg(0xA0,0x0000);
-  	delay(20);
-  }
-  else if(driverCode == 0x9325)		 //2.8" TFT LCD Module,DriverIC is ILI9325
-  {
-  	    wr_reg(0x00e7,0x0010);      
-        wr_reg(0x0000,0x0001);  			//start internal osc
-        wr_reg(0x0001,0x0100);     
-        wr_reg(0x0002,0x0700); 				//power on sequence                     
-        wr_reg(0x0003,(1<<12)|(1<<5)|(1<<4) ); 	//65K 
-        wr_reg(0x0004,0x0000);                                   
-        wr_reg(0x0008,0x0207);	           
-        wr_reg(0x0009,0x0000);         
-        wr_reg(0x000a,0x0000); 				//display setting         
-        wr_reg(0x000c,0x0001);				//display setting          
-        wr_reg(0x000d,0x0000); 				//0f3c          
-        wr_reg(0x000f,0x0000);
-//Power On sequence //
-        wr_reg(0x0010,0x0000);   
-        wr_reg(0x0011,0x0007);
-        wr_reg(0x0012,0x0000);                                                                 
-        wr_reg(0x0013,0x0000);                 
-        delay(15);
-        wr_reg(0x0010,0x1590);   
-        wr_reg(0x0011,0x0227);
-        delay(15);
-        wr_reg(0x0012,0x009c);                  
-        delay(15);
-        wr_reg(0x0013,0x1900);   
-        wr_reg(0x0029,0x0023);
-        wr_reg(0x002b,0x000e);
-        delay(15);
-        wr_reg(0x0020,0x0000);                                                            
-        wr_reg(0x0021,0x0000);           
-///////////////////////////////////////////////////////      
-        delay(15);
-        wr_reg(0x0030,0x0007); 
-        wr_reg(0x0031,0x0707);   
-        wr_reg(0x0032,0x0006);
-        wr_reg(0x0035,0x0704);
-        wr_reg(0x0036,0x1f04); 
-        wr_reg(0x0037,0x0004);
-        wr_reg(0x0038,0x0000);        
-        wr_reg(0x0039,0x0706);     
-        wr_reg(0x003c,0x0701);
-        wr_reg(0x003d,0x000f);
-        delay(15);
-        wr_reg(0x0050,0x0000);        
-        wr_reg(0x0051,0x00ef);   
-        wr_reg(0x0052,0x0000);     
-        wr_reg(0x0053,0x013f);
-        wr_reg(0x0060,0xa700);        
-        wr_reg(0x0061,0x0001); 
-        wr_reg(0x006a,0x0000);
-        wr_reg(0x0080,0x0000);
-        wr_reg(0x0081,0x0000);
-        wr_reg(0x0082,0x0000);
-        wr_reg(0x0083,0x0000);
-        wr_reg(0x0084,0x0000);
-        wr_reg(0x0085,0x0000);
-      
-        wr_reg(0x0090,0x0010);     
-        wr_reg(0x0092,0x0000);  
-        wr_reg(0x0093,0x0003);
-        wr_reg(0x0095,0x0110);
-        wr_reg(0x0097,0x0000);        
-        wr_reg(0x0098,0x0000);  
-         //display on sequence     
-        wr_reg(0x0007,0x0133);
+  delay(15);
+  wr_reg(0x0030,0x0007); 
+  wr_reg(0x0031,0x0707);   
+  wr_reg(0x0032,0x0006);
+  wr_reg(0x0035,0x0704);
+  wr_reg(0x0036,0x1f04); 
+  wr_reg(0x0037,0x0004);
+  wr_reg(0x0038,0x0000);        
+  wr_reg(0x0039,0x0706);     
+  wr_reg(0x003c,0x0701);
+  wr_reg(0x003d,0x000f);
+  delay(15);
+  wr_reg(0x0050,0x0000);        
+  wr_reg(0x0051,0x00ef);   
+  wr_reg(0x0052,0x0000);     
+  wr_reg(0x0053,0x013f);
+  wr_reg(0x0060,0xa700);        
+  wr_reg(0x0061,0x0001); 
+  wr_reg(0x006a,0x0000);
+  wr_reg(0x0080,0x0000);
+  wr_reg(0x0081,0x0000);
+  wr_reg(0x0082,0x0000);
+  wr_reg(0x0083,0x0000);
+  wr_reg(0x0084,0x0000);
+  wr_reg(0x0085,0x0000);
+     
+  wr_reg(0x0090,0x0010);     
+  wr_reg(0x0092,0x0000);  
+  wr_reg(0x0093,0x0003);
+  wr_reg(0x0095,0x0110);
+  wr_reg(0x0097,0x0000);        
+  wr_reg(0x0098,0x0000);  
+  //display on sequence     
+  wr_reg(0x0007,0x0133);
     
-        wr_reg(0x0020,0x0000);                                                            
-        wr_reg(0x0021,0x0000);
-   }
-  else if(driverCode == 0x9320)		 //3.2" TFT LCD Module,DriverIC is ILI9320
-  {
-    /* Start Initial Sequence --------------------------------------------------*/
-	wr_reg(0xE5, 0x8000);                 /* Set the internal vcore voltage     */
-	wr_reg(0x00, 0x0001);                 /* Start internal OSC                 */
-	wr_reg(0x01, 0x0100);                 /* Set SS and SM bit                  */
-	wr_reg(0x02, 0x0700);                 /* Set 1 line inversion               */
-	wr_reg(0x03, 0x1030);                 /* Set GRAM write direction and BGR=1 */
-	wr_reg(0x04, 0x0000);                 /* Resize register                    */
-	wr_reg(0x08, 0x0202);                 /* 2 lines each, back and front porch */
-	wr_reg(0x09, 0x0000);                 /* Set non-disp area refresh cyc ISC  */
-	wr_reg(0x0A, 0x0000);                 /* FMARK function                     */
-	wr_reg(0x0C, 0x0000);                 /* RGB interface setting              */
-	wr_reg(0x0D, 0x0000);                 /* Frame marker Position              */
-	wr_reg(0x0F, 0x0000);                 /* RGB interface polarity             */
-	
-	/* Power On sequence -------------------------------------------------------*/
-	wr_reg(0x10, 0x0000);                 /* Reset Power Control 1              */
-	wr_reg(0x11, 0x0000);                 /* Reset Power Control 2              */
-	wr_reg(0x12, 0x0000);                 /* Reset Power Control 3              */
-	wr_reg(0x13, 0x0000);                 /* Reset Power Control 4              */
-	delay(20);                            /* Discharge cap power voltage (200ms)*/
-    wr_reg(0x10, 0x17B0);                 /* SAP, BT[3:0], AP, DSTB, SLP, STB   */
-	wr_reg(0x11, 0x0137);                 /* DC1[2:0], DC0[2:0], VC[2:0]        */
-	delay(5);                             /* Delay 50 ms                        */
-	wr_reg(0x12, 0x0139);                 /* VREG1OUT voltage                   */
-	delay(5);                             /* Delay 50 ms                        */
-	wr_reg(0x13, 0x1D00);                 /* VDV[4:0] for VCOM amplitude        */
-	wr_reg(0x29, 0x0013);                 /* VCM[4:0] for VCOMH                 */
-	delay(5);                             /* Delay 50 ms                        */
-	wr_reg(0x20, 0x0000);                 /* GRAM horizontal Address            */
-	wr_reg(0x21, 0x0000);                 /* GRAM Vertical Address              */
-	
-	/* Adjust the Gamma Curve --------------------------------------------------*/
-	wr_reg(0x30, 0x0006);
-	wr_reg(0x31, 0x0101);
-	wr_reg(0x32, 0x0003);
-	wr_reg(0x35, 0x0106);
-	wr_reg(0x36, 0x0B02);
-	wr_reg(0x37, 0x0302);
-	wr_reg(0x38, 0x0707);
-	wr_reg(0x39, 0x0007);
-	wr_reg(0x3C, 0x0600);
-	wr_reg(0x3D, 0x020B);
-	  
-	/* Set GRAM area -----------------------------------------------------------*/
-	wr_reg(0x50, 0x0000);                 /* Horizontal GRAM Start Address      */
-	wr_reg(0x51, (HEIGHT-1));             /* Horizontal GRAM End   Address      */
-	wr_reg(0x52, 0x0000);                 /* Vertical   GRAM Start Address      */
-	wr_reg(0x53, (WIDTH-1));              /* Vertical   GRAM End   Address      */
-	wr_reg(0x60, 0x2700);                 /* Gate Scan Line                     */
-	wr_reg(0x61, 0x0001);                 /* NDL,VLE, REV                       */
-	wr_reg(0x6A, 0x0000);                 /* Set scrolling line                 */
-	
-	/* Partial Display Control -------------------------------------------------*/
-	wr_reg(0x80, 0x0000);
-	wr_reg(0x81, 0x0000);
-	wr_reg(0x82, 0x0000);
-	wr_reg(0x83, 0x0000);
-	wr_reg(0x84, 0x0000);
-	wr_reg(0x85, 0x0000);
-	
-	/* Panel Control -----------------------------------------------------------*/
-	wr_reg(0x90, 0x0010);
-	wr_reg(0x92, 0x0000);
-	wr_reg(0x93, 0x0003);
-	wr_reg(0x95, 0x0110);
-	wr_reg(0x97, 0x0000);
-	wr_reg(0x98, 0x0000);
-  }
-  
+  wr_reg(0x0020,0x0000);                                                            
+  wr_reg(0x0021,0x0000);
   /* Set GRAM write direction and BGR = 1
      I/D=10 (Horizontal : increment, Vertical : increment)
      AM=1 (address is updated in vertical writing direction)                  */
   wr_reg(0x03, 0x1038);
 
   wr_reg(0x07, 0x0173);                 /* 262K color and display ON          */ 
-  if(driverCode == 0x8989)		 //3.2" TFT LCD Module,DriverIC is SSD1289
-  {
-  	    wr_reg(0x0000,0x0001);    delay(5);  
-    	wr_reg(0x0003,0xA8A4);    delay(5);   
-    	wr_reg(0x000C,0x0000);    delay(5);   
-    	wr_reg(0x000D,0x080C);    delay(5);   
-    	wr_reg(0x000E,0x2B00);    delay(5);   
-    	wr_reg(0x001E,0x00B0);    delay(5);   
-    	wr_reg(0x0001,0x2b3F);    delay(5);        
-    	wr_reg(0x0002,0x0600);    delay(5);
-    	wr_reg(0x0010,0x0000);    delay(5);
-    	wr_reg(0x0011,0x6078);    delay(5);        
-    	wr_reg(0x0005,0x0000);    delay(5);
-    	wr_reg(0x0006,0x0000);    delay(5);
-    	wr_reg(0x0016,0xEF1C);    delay(5);
-    	wr_reg(0x0017,0x0003);    delay(5);
-    	wr_reg(0x0007,0x0233);    delay(5);               
-    	wr_reg(0x000B,0x0000);    delay(5);
-    	wr_reg(0x000F,0x0000);    delay(5);       
-    	wr_reg(0x0041,0x0000);    delay(5);
-    	wr_reg(0x0042,0x0000);    delay(5);
-    	wr_reg(0x0048,0x0000);    delay(5);
-    	wr_reg(0x0049,0x013F);    delay(5);
-    	wr_reg(0x004A,0x0000);    delay(5);
-    	wr_reg(0x004B,0x0000);    delay(5);
-    	wr_reg(0x0044,0xEF00);    delay(5);
-    	wr_reg(0x0045,0x0000);    delay(5);
-    	wr_reg(0x0046,0x013F);    delay(5);
-    	wr_reg(0x0030,0x0707);    delay(5);
-    	wr_reg(0x0031,0x0204);    delay(5);
-    	wr_reg(0x0032,0x0204);    delay(5);
-    	wr_reg(0x0033,0x0502);    delay(5);
-    	wr_reg(0x0034,0x0507);    delay(5);
-    	wr_reg(0x0035,0x0204);    delay(5);
-    	wr_reg(0x0036,0x0204);    delay(5);
-    	wr_reg(0x0037,0x0502);    delay(5);
-    	wr_reg(0x003A,0x0302);    delay(5);
-    	wr_reg(0x003B,0x0302);    delay(5);
-    	wr_reg(0x0023,0x0000);    delay(5);
-    	wr_reg(0x0024,0x0000);    delay(5);
-    	wr_reg(0x0025,0x8000);    delay(5);
-    	wr_reg(0x004f,0);        
-    	wr_reg(0x004e,0);        
-  }
 }
 
 
@@ -506,21 +289,12 @@ void GLCD_Init (void) {
 *******************************************************************************/
 
 void GLCD_WindowMax (void) {
-  
-  if(driverCode==0x8989)
-  {
-	wr_reg(0x44, 0);                      /* Horizontal GRAM Start Address      */
-    wr_reg(0x44, 0 |((HEIGHT-1)<<8));     /* Horizontal GRAM End   Address (-1) */
-    wr_reg(0x45, 0);                      /* Vertical   GRAM Start Address      */
-    wr_reg(0x46, WIDTH-1);                /* Vertical   GRAM End   Address (-1) */
-  }
-  else
-  {
+	
   	wr_reg(0x50, 0);                      /* Horizontal GRAM Start Address      */
   	wr_reg(0x51, HEIGHT-1);               /* Horizontal GRAM End   Address (-1) */
   	wr_reg(0x52, 0);                      /* Vertical   GRAM Start Address      */
   	wr_reg(0x53, WIDTH-1);                /* Vertical   GRAM End   Address (-1) */
-  }
+
 }
 
 
@@ -535,16 +309,9 @@ void GLCD_PutPixel (unsigned int x, unsigned int y) {
   
   GLCD_WindowMax();
 
-  if(driverCode==0x8989)
-  {
-  	wr_reg(0x4e, y);
-  	wr_reg(0x4f, WIDTH-1-x);
-  }
-  else
-  {
-  	wr_reg(0x20, y);
-  	wr_reg(0x21, WIDTH-1-x);
-  }
+  wr_reg(0x20, y);
+  wr_reg(0x21, WIDTH-1-x);
+  
   LCD_CS(0)
   wr_cmd(0x22);
   wr_dat(TextColor);
@@ -587,16 +354,9 @@ void GLCD_Clear (unsigned short color) {
 
   GLCD_WindowMax();
 
-  if(driverCode==0x8989)
-  {
-  	wr_reg(0x4e, 0);
-  	wr_reg(0x4f, 0);
-  }
-  else
-  {
-  	wr_reg(0x20, 0);
-  	wr_reg(0x21, 0);
-  }
+  wr_reg(0x20, 0);
+  wr_reg(0x21, 0);
+
   LCD_CS(0)
   wr_cmd(0x22);
   wr_dat_start();
@@ -619,26 +379,15 @@ void GLCD_DrawChar (unsigned int x, unsigned int y, unsigned short *c) {
 
   x = WIDTH-x-CHAR_W;
 
-  if(driverCode==0x8989)
-  {
-  	wr_reg(0x44, y);                      /* Horizontal GRAM Start Address      */
-    wr_reg(0x44, y |((y+CHAR_H-1)<<8));   /* Horizontal GRAM End   Address (-1) */
-    wr_reg(0x45, x);                      /* Vertical   GRAM Start Address      */
-    wr_reg(0x46, x+CHAR_W-1);             /* Vertical   GRAM End   Address (-1) */
-
-	wr_reg(0x4e, y);
-  	wr_reg(0x4f, x);
-  }
-  else
-  {
-  	wr_reg(0x50, y);                      /* Horizontal GRAM Start Address      */
-  	wr_reg(0x51, y+CHAR_H-1);             /* Horizontal GRAM End   Address (-1) */
-  	wr_reg(0x52, x);                      /* Vertical   GRAM Start Address      */
-  	wr_reg(0x53, x+CHAR_W-1);             /* Vertical   GRAM End   Address (-1) */
+  
+  wr_reg(0x50, y);                      /* Horizontal GRAM Start Address      */
+  wr_reg(0x51, y+CHAR_H-1);             /* Horizontal GRAM End   Address (-1) */
+  wr_reg(0x52, x);                      /* Vertical   GRAM Start Address      */
+  wr_reg(0x53, x+CHAR_W-1);             /* Vertical   GRAM End   Address (-1) */
 
 	wr_reg(0x20, y);
-  	wr_reg(0x21, x);
-  }
+  wr_reg(0x21, x);
+	
   LCD_CS(0)
   wr_cmd(0x22);
   wr_dat_start();
